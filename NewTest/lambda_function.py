07c87f5d38d41c046e078c0820f39e0be403bf46
NikhitaB67 <43357389+NikhitaB67@users.noreply.github.com>
2021-09-14 11:44:15 -0700

    commenting out the print line

--- NewTest/lambda_function.py
+++ NewTest/lambda_function.py
@@ -3,7 +3,7 @@ import requests
 
 def lambda_handler(event, context):
     # TODO implement
-    print("Hey")
+    # print("Hey")
     print("It's working!!")
     return {
         'statusCode': 200,
